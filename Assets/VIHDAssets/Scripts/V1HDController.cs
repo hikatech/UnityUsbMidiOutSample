bb32883869dc7df73530da1863e4735240fd3777
hikatech <11090707+hikatech@users.noreply.github.com>
2018-06-14 01:47:36 +0900

    - first commit

---
+++ Assets/VIHDAssets/Scripts/V1HDController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class V1HDController : MonoBehaviour {
+
+    void Start () {
+        MidiBridge.instance.Warmup();
+        Debug.Log("*** input selecttion:  'A'=A-1  |  'S'=A-2***");
+    }
+
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            // source select A-1
+            MidiOut.SendControlChange(MidiChannel.Ch1, 0, 0);
+            MidiOut.SendControlChange(MidiChannel.Ch1, 32, 0);
+            MidiOut.SendProgramChange(MidiChannel.Ch1, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            // source select A-2
+            MidiOut.SendControlChange(MidiChannel.Ch1, 0, 0);
+            MidiOut.SendControlChange(MidiChannel.Ch1, 32, 0);
+            MidiOut.SendProgramChange(MidiChannel.Ch1, 1, 0);
+        }
+    }
+}
